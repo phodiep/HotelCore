78b8233a9b2d7c1c37a868dd2d3b7c360d9985db
Pho Diep <phodiep@gmail.com>
2015-02-09 14:05:13 -0800

    init commit

---
+++ HotelCore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HotelCore
+//
+//  Created by Pho Diep on 2/9/15.
+//  Copyright (c) 2015 Pho Diep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
